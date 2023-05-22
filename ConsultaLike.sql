372d3a7e9849a2c04966124730476b753cd4e7b4
marianelopes <104696075+Marianelopes84@users.noreply.github.com>
2023-05-22 16:13:55 -0300

    Update ConsultaLike.sql

--- ConsultaLike.sql
+++ ConsultaLike.sql
@@ -4,4 +4,4 @@ SELECT * FROM tabela_de_produtos WHERE SABOR LIKE '%Maça%'
 AND EMBALAGEM = 'PET';
 
 --Quantos clientes possuem o último sobrenome Mattos?
-SELECT * FROM tabela_de_clientes WHERE NOME LIKE '%Mattos'
+SELECT * FROM tabela_de_clientes WHERE NOME LIKE '%Mattos';
