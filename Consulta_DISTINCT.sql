c799c8e6459899083012da12039dfaf9a3985fb4
marianelopes <104696075+Marianelopes84@users.noreply.github.com>
2023-05-22 16:14:13 -0300

    Update Consulta_DISTINCT.sql

--- Consulta_DISTINCT.sql
+++ Consulta_DISTINCT.sql
@@ -17,4 +17,4 @@ SELECT DISTINCT EMBALAGEM, TAMANHO, SABOR FROM tabela_de_produtos;
 
 
 -- Quais são os bairros da cidade do Rio de Janeiro que possuem clientes?
-SELECT DISTINCT BAIRRO FROM tabela_de_clientes WHERE CIDADE = 'Rio de Janeiro'
+SELECT DISTINCT BAIRRO FROM tabela_de_clientes WHERE CIDADE = 'Rio de Janeiro';
